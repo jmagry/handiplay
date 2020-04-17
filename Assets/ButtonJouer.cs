ffe9236b6179b85286074981c586f8d3fc11d0eb
Léo <leo.philip33.bordeaux@gmail.com>
2020-04-17 16:47:38 +0100

    menu

---
+++ Assets/ButtonJouer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonJouer : MonoBehaviour
+{
+    public GameObject hideOptions;
+    public GameObject hideJouer;
+    public GameObject hideCredits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClick()
+    {
+        hideOptions.SetActive(false);
+        hideJouer.SetActive(false);
+        hideCredits.SetActive(false);
+    }
+}

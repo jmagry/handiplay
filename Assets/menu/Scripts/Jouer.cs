4ac3c265a71eb6c8b36229f76ebf048495d78c39
Léo <leo.philip33.bordeaux@gmail.com>
2020-04-22 21:42:05 +0100

    MenuPlay

---
+++ Assets/menu/Scripts/Jouer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jouer : MonoBehaviour
+{
+    public string changeSceneGame;
+
+    public void changeScene(){          
+        SceneManager.LoadScene(changeSceneGame); //On change de scène
+    }
+
+}
